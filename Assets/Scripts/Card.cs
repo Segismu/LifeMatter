5a7153eb5975a565f7a6f434afb7e833b9dfe348
Prometeo <105377032+Segismu@users.noreply.github.com>
2022-09-26 16:40:29 -0300

    Base scriptable objects added.

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    public CardScriptableObject cardSO;
+
+    public int currentHP, attackPower, manaCost;
+
+    public TMP_Text hpText, attackText, costText, nameText, actionText, loreText;
+
+    public Image characterArt, bgArt;
+
+    void Start()
+    {
+        SetupCard();
+    }
+
+    private void SetupCard()
+    {
+        currentHP = cardSO.currentHP;
+        attackPower = cardSO.attackPower;
+        manaCost = cardSO.manaCost;
+
+        hpText.text = currentHP.ToString();
+        attackText.text = attackPower.ToString();
+        costText.text = manaCost.ToString();
+
+        nameText.text = cardSO.cardName;
+        actionText.text = cardSO.actionDescription;
+        loreText.text = cardSO.cardLore;
+
+        characterArt.sprite = cardSO.characterSprite;
+        bgArt.sprite = cardSO.bgSprite;
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
