5224b7460be57ddbfc8276b5b9334cc37e3650fc
Prometeo <105377032+Segismu@users.noreply.github.com>
2022-09-28 15:20:49 -0300

    Base deck logic.

---
+++ Assets/Scripts/DeckController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class DeckController : MonoBehaviour
+{
+    public static DeckController instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public List<CardScriptableObject> deckToUse = new List<CardScriptableObject>();
+
+    private List<CardScriptableObject> activeCards = new List<CardScriptableObject>();
+
+    void Start()
+    {
+        SetupDeck();
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    private void SetupDeck()
+    {
+        activeCards.Clear();
+        List<CardScriptableObject> tempDeck = new List<CardScriptableObject>();
+        tempDeck.AddRange(deckToUse);
+
+        int iterations = 0;
+        while(tempDeck.Count > 0 && iterations < 300)
+        {
+            int selected = Random.Range(0, tempDeck.Count);
+            activeCards.Add(tempDeck[selected]);
+            tempDeck.RemoveAt(selected);
+
+            iterations++;
+        }
+    }
+}
